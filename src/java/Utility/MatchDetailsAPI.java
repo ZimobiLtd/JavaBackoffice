3179e177b3df5034c26d17c6b8898394585e5fe2
JacGitau <mwangicuesta@gmail.com>
2021-07-09 14:45:30 +0300

    20207-09 >2

---
+++ src/java/Utility/MatchDetailsAPI.java
@@ -0,0 +1,128 @@
+package Utility;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ *
+ * @author jac
+ */
+@WebServlet(urlPatterns = {"/matchdetails"})
+public class MatchDetailsAPI extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    Connection conn;
+    String response,username ,password,function,maindata;
+    JSONObject jsonobj=null;JSONArray responseobj  = null;
+    public static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+        resp.setContentType("text/json;charset=UTF-8");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "POST");
+      
+        String matchID=req.getParameter("match_id").trim();
+        String task=req.getParameter("task").trim();
+        
+        if(task.equalsIgnoreCase("get"))
+        {
+            responseobj=new MatchDetails().getMatchDetails(matchID);
+        }
+        else if(task.equalsIgnoreCase("remove"))
+        {
+            responseobj=new MatchDetails().removeGameByMatchID(matchID);
+        }
+        else
+        {
+            try 
+            {
+                jsonobj= new JSONObject();
+                jsonobj.put("Message", "Match not found");
+                
+                responseobj=new JSONArray().put(jsonobj);
+            } 
+            catch (JSONException ex) 
+            {
+                System.out.println("Error=== "+ex.getMessage());
+            }
+            
+            
+        }
+        
+        PrintWriter out = resp.getWriter(); 
+        out.print(responseobj);
+    }
+
+    
+    
+    
+
+    
+    
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
